06255577c25990e2b6d4b9eef2033834bb4cfc1a
Isak15 <nilisak@student.chalmers.se>
2021-11-03 14:25:54 +0100

    hejade

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World! Forza chalmers");
+        System.out.println("Hello World! Forza chalmers. heja");
     }
 }
